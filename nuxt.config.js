dd454951d9d51f83fa6f46182e99c1dde21153ee
Mitch Maifeld <mitch@maifeld.name>
2023-04-19 10:30:55 -0500

    name and front image

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
-const name = 'Janet Doe'
-const dates = 'July 29th 1958 - Jan 4th 2021'
+const name = 'Kenneth Monroe Anderson'
+const dates = 'March 9th, 1939 - November 2nd 2022'
 
 export default {
   publicRuntimeConfig: {
