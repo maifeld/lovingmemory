85d4dfe538777652dac98ee74087550675f2bc80
Mitch Maifeld <mitch@maifeld.name>
2023-04-21 15:08:42 -0500

    Clear out Memories

--- pages/memories.vue
+++ pages/memories.vue
@@ -7,33 +7,6 @@
       <!-- CONTENT START -->
       <h1 class="title">Memories</h1>
 	  <p class="highlight">Please share your memories of Ken to this address and we will post them here.</p>
-      <h2>Service <span>4 - 7 pm</span></h2>
-      <ul class="fa-ul">
-        <ListItem>Service starts at 10:30am at Church Address</ListItem>
-        <ListItem>Entrance music: And Can It Be</ListItem>
-        <ListItem>Scripture reading</ListItem>
-        <ListItem>Welcome &amp; Opening Prayer</ListItem>
-        <ListItem>Song: Goodness of God</ListItem>
-        <ListItem>Tributes - Family &amp; Friends</ListItem>
-        <ListItem>Few moments of quiet</ListItem>
-        <ListItem>Short message</ListItem>
-        <ListItem>Prayer of Commendation</ListItem>
-        <ListItem>Closing prayer &amp; Grace blessing</ListItem>
-        <ListItem>Exit music: New Wine</ListItem>
-      </ul>
-      <h2>Crematorium <span>12:00pm</span></h2>
-      <ul class="fa-ul">
-        <ListItem>Service starts at 12pm at Crematorium address</ListItem>
-        <ListItem>Entrance music: And Can It Be</ListItem>
-        <ListItem>Scripture reading - Psalm 61 v1-4</ListItem>
-        <ListItem>Welcome &amp; Opening Prayer</ListItem>
-        <ListItem>Tribute</ListItem>
-        <ListItem>Few moments of quiet</ListItem>
-        <ListItem>Short message</ListItem>
-        <ListItem>Committal</ListItem>
-        <ListItem>Closing prayer &amp; Grace blessing</ListItem>
-        <ListItem>Exit music: New Wine</ListItem>
-      </ul>
       <!-- CONTENT END -->
     </div>
   </div>
