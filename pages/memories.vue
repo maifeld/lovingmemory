5b9211e54e4e387241dc05d1c99b19c13bec8fda
Mitch Maifeld <mitch@maifeld.name>
2023-05-16 05:37:04 -0500

    email address for memories and yarn upgrade

--- pages/memories.vue
+++ pages/memories.vue
@@ -6,7 +6,16 @@
     <div class="section details">
       <!-- CONTENT START -->
       <h1 class="title">Memories</h1>
-	  <p class="highlight">Please share your memories of Ken to this address and we will post them here.</p>
+	  <p>Please share your memories of Ken to this address and we will post them here. <br> email: <a href="mailto:memories@maifeld.name?subject=My%20Memories%20of%20Ken&amp;body=I%20give%20you%20permission%20to%20put%20this%20online:">memories@maifeld.name</a>
+	  </p>
+	  <p style="text-align:right;" class="highlight">
+	  I remember when Amy and I broke the news to Ken that he was going to be a grandfather, we gifted him a bottle of "Old Grandad" whiskey.  He got the joke after some nudging!<br><br>
+	  <em>Mitch Maifeld</em>
+	  </p>
+	  <p style="text-align:right;" class="highlight">
+	  I remember visiting the art museum in Chicago and Ken telling about <i>trompe l'oeil</i>.  I sort of recalled it through taking art class with him in high school, but I felt pretty lucky to be touring an art museum with <b>two</b> teachers of art!<br><br>
+	  <em>Mitch Maifeld</em>
+	  </p>
       <!-- CONTENT END -->
     </div>
   </div>
