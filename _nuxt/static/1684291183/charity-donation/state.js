f40a1411250b081ff32b1a64b4390273a713e414
maifeld <maifeld@users.noreply.github.com>
2023-05-19 20:59:30 +0000

    deploy: 2127c2e57e25058e0a05d3d0ac4ac679fa210566

--- _nuxt/static/1684291183/charity-donation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1684291183",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcharity-donation",config:{name:"Kenneth Monroe Anderson",dates:"March 9, 1939 - November 2, 2022",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
