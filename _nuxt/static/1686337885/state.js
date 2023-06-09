33869b99ee4e8d685ade50195daedb0267b20109
maifeld <maifeld@users.noreply.github.com>
2023-06-09 19:38:58 +0000

    deploy: 3f7e2c3f6fe8925f6111fac9468adf765091ed59

--- _nuxt/static/1686337885/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1686337885",layout:"default",error:a,serverRendered:true,routePath:b,config:{name:"Kenneth Monroe Anderson",dates:"March 9, 1939 - November 2, 2022",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
